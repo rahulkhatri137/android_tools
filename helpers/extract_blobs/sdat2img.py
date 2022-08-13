87819e7f1684156c287b29f3628f45e5028f8910
Sourcery AI <>
2022-08-13 15:51:55 +0000

    'Refactored by Sourcery'

--- helpers/extract_blobs/sdat2img.py
+++ helpers/extract_blobs/sdat2img.py
@@ -12,6 +12,8 @@ import sys, os, errno
 def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
     __version__ = '1.2'
 
+    __version__ = '1.2'
+
     if sys.hexversion < 0x02070000:
         print >> sys.stderr, "Python 2.7 or newer is required."
         try:
@@ -29,38 +31,36 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
             print('Error on parsing following data to rangeset:\n{}'.format(src), file=sys.stderr)
             sys.exit(1)
 
-        return tuple ([ (num_set[i], num_set[i+1]) for i in range(1, len(num_set), 2) ])
+        return tuple((num_set[i], num_set[i+1]) for i in range(1, len(num_set), 2))
 
     def parse_transfer_list_file(path):
-        trans_list = open(TRANSFER_LIST_FILE, 'r')
+        with open(TRANSFER_LIST_FILE, 'r') as trans_list:
+            # First line in transfer list is the version number
+            version = int(trans_list.readline())
 
-        # First line in transfer list is the version number
-        version = int(trans_list.readline())
+            # Second line in transfer list is the total number of blocks we expect to write
+            new_blocks = int(trans_list.readline())
 
-        # Second line in transfer list is the total number of blocks we expect to write
-        new_blocks = int(trans_list.readline())
+            if version >= 2:
+                # Third line is how many stash entries are needed simultaneously
+                trans_list.readline()
+                # Fourth line is the maximum number of blocks that will be stashed simultaneously
+                trans_list.readline()
 
-        if version >= 2:
-            # Third line is how many stash entries are needed simultaneously
-            trans_list.readline()
-            # Fourth line is the maximum number of blocks that will be stashed simultaneously
-            trans_list.readline()
+            # Subsequent lines are all individual transfer commands
+            commands = []
+            for line in trans_list:
+                line = line.split(' ')
+                cmd = line[0]
+                if cmd in ['erase', 'new', 'zero']:
+                    commands.append([cmd, rangeset(line[1])])
+                else:
+                    # Skip lines starting with numbers, they are not commands anyway
+                    if not cmd[0].isdigit():
+                        print('Command "{}" is not valid.'.format(cmd), file=sys.stderr)
+                        trans_list.close()
+                        sys.exit(1)
 
-        # Subsequent lines are all individual transfer commands
-        commands = []
-        for line in trans_list:
-            line = line.split(' ')
-            cmd = line[0]
-            if cmd in ['erase', 'new', 'zero']:
-                commands.append([cmd, rangeset(line[1])])
-            else:
-                # Skip lines starting with numbers, they are not commands anyway
-                if not cmd[0].isdigit():
-                    print('Command "{}" is not valid.'.format(cmd), file=sys.stderr)
-                    trans_list.close()
-                    sys.exit(1)
-
-        trans_list.close()
         return version, new_blocks, commands
 
     BLOCK_SIZE = 4096
@@ -89,34 +89,33 @@ def main(TRANSFER_LIST_FILE, NEW_DATA_FILE, OUTPUT_IMAGE_FILE):
         else:
             raise
 
-    new_data_file = open(NEW_DATA_FILE, 'rb')
-    all_block_sets = [i for command in commands for i in command[1]]
-    max_file_size = max(pair[1] for pair in all_block_sets)*BLOCK_SIZE
+    with open(NEW_DATA_FILE, 'rb') as new_data_file:
+        all_block_sets = [i for command in commands for i in command[1]]
+        max_file_size = max(pair[1] for pair in all_block_sets)*BLOCK_SIZE
 
-    for command in commands:
-        if command[0] == 'new':
-            for block in command[1]:
-                begin = block[0]
-                end = block[1]
-                block_count = end - begin
-                print('Copying {} blocks into position {}...'.format(block_count, begin))
+        for command in commands:
+            if command[0] == 'new':
+                for block in command[1]:
+                    begin = block[0]
+                    end = block[1]
+                    block_count = end - begin
+                    print('Copying {} blocks into position {}...'.format(block_count, begin))
 
-                # Position output file
-                output_img.seek(begin*BLOCK_SIZE)
+                    # Position output file
+                    output_img.seek(begin*BLOCK_SIZE)
 
-                # Copy one block at a time
-                while(block_count > 0):
-                    output_img.write(new_data_file.read(BLOCK_SIZE))
-                    block_count -= 1
-        else:
-            print('Skipping command {}...'.format(command[0]))
+                    # Copy one block at a time
+                    while(block_count > 0):
+                        output_img.write(new_data_file.read(BLOCK_SIZE))
+                        block_count -= 1
+            else:
+                print('Skipping command {}...'.format(command[0]))
 
-    # Make file larger if necessary
-    if(output_img.tell() < max_file_size):
-        output_img.truncate(max_file_size)
+        # Make file larger if necessary
+        if(output_img.tell() < max_file_size):
+            output_img.truncate(max_file_size)
 
-    output_img.close()
-    new_data_file.close()
+        output_img.close()
     print('Done! Output image: {}'.format(os.path.realpath(output_img.name)))
 
 if __name__ == '__main__':
